7f147cdb3084f7156d054aa6526922bd481d6bb3
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-04-10 09:28:24 +0900

    feat: headerにContent-Typeを追加

--- frontend/src/lib/fetcher.ts
+++ frontend/src/lib/fetcher.ts
@@ -1,7 +1,12 @@
-export const fetcher = async <T>(resource: string, init?: RequestInit): Promise<T> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}${resource}`,
-    init
-    )
+export const fetcher = async <T>(
+  resource: string,
+  init?: RequestInit
+): Promise<T> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${resource}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...init,
+  });
   return res.json();
 };
